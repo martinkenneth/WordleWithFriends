c0056e1b34c9db03a76d120a596d05614bdb39f7
Kenneth Martin <martinckenneth@gmail.com>
2022-04-26 12:02:35 -0700

    Testing2

--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -5,6 +5,7 @@ import Header from "../components/Header";
 const Main = () => {
     return (
         <div>
+            <h1>Testing pls work</h1>
             <Header />
             <CreatorForm />
         </div>
