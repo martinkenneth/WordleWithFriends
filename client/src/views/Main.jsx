099124ff0c4a961eea281bd558bd2d5de8a83511
Kenneth Martin <martinckenneth@gmail.com>
2022-04-26 11:56:34 -0700

    update

--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -5,7 +5,6 @@ import Header from "../components/Header";
 const Main = () => {
     return (
         <div>
-            <h1>Test</h1>
             <Header />
             <CreatorForm />
         </div>
