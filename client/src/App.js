3259493a4186e168a9731ff664c2bbc26a79c1c3
Kenneth Martin <martinckenneth@gmail.com>
2022-04-27 14:39:43 -0700

    creatorForm update

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,19 @@
-
-import './App.css';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import Main from './views/Main';
+import "./App.css";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Main from "./views/Main";
 
 function App() {
-  return (
-    <Switch>
-      <Route path="/examples">
-        <Main />
-      </Route>
-    {/* you can also use redirect to force a route to another route */}
-      <Route path="/">
-        <Redirect to='/examples' />
-      </Route>
-    </Switch>
-  );
+    return (
+        <Switch>
+            <Route path="/examples">
+                <Main />
+            </Route>
+            {/* you can also use redirect to force a route to another route */}
+            <Route path="/">
+                <Redirect to="/examples" />
+            </Route>
+        </Switch>
+    );
 }
 
 export default App;
