dac0a3f5c60f37918c6b9c4e58bf53a1ee0e1abf
Kenneth Martin <martinckenneth@gmail.com>
2022-04-28 16:54:46 -0700

    update

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,10 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Modal.module.css";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
-const Modal = () => {
+const Modal = (props) => {
+    const [guesserName, setGuesserName] = useState("");
+    const [error, setError] = useState(false);
+    const history = useHistory();
+    // const [score, setScore] = useState(5);
+    const refreshPage = () => {
+        window.location.reload(false);
+    };
+    // name=Ryan Renolds&attempts=4&creator_id=98
+    /*=========================================================================
+        Handle API Request with AXIOS
+    =========================================================================*/
+    const postAPI = () => {
+        axios
+            .post(`http://localhost:8080/api/guessers`, null, {
+                params: {
+                    name: guesserName,
+                    attempts: props.score,
+                    creator_id: props.creatorId,
+                },
+            })
+            .then((response) => {
+                // console.log("Creator ID:", response.data.id);
+                console.log(response);
+                // history.push(`/`);
+                // refreshPage();
+            })
+            .catch((err) => {
+                console.log("ERROR", err);
+            });
+    };
+
+    const handleSubmission = (e) => {
+        e.preventDefault();
+        let errorFlag = false;
+        if (!guesserName) {
+            errorFlag = true;
+        } else {
+            postAPI();
+            history.push(`/`);
+            refreshPage();
+        }
+        setError(errorFlag);
+    };
+
     return (
         <div className={styles.Modal}>
-            <h1>Hello</h1>
+            <h1>Thanks for playing!</h1>
+            <h3>Score: {props.score}</h3>
+            <form onSubmit={handleSubmission} autoComplete="off">
+                <input
+                    type="text"
+                    name="name"
+                    value={guesserName}
+                    onChange={(e) =>
+                        setGuesserName(e.target.value.toUpperCase())
+                    }
+                    placeholder="Enter Name"
+                />
+                {error ? (
+                    <p className={styles.errorMsg}>Please enter a name.</p>
+                ) : (
+                    <></>
+                )}
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
