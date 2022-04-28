dca511756a0092a757197a9da3788141fed8830b
Kenneth Martin <martinckenneth@gmail.com>
2022-04-27 20:28:11 -0700

    creator object to encryptedURL

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,30 @@ import CreatorView from "./views/CreatorView";
 
 function App() {
     return (
+        // Testing Hashed URLs ======================================
         <Switch>
-            <Route exact path="/:id/:name=:word/guess">
+            <Route exact path="/playgame/:encryptedObj">
                 <GuesserView />
             </Route>
-            <Route exact path="/:id/:name=:word">
+            <Route exact path="/creator/:encryptedObj">
                 <CreatorView />
             </Route>
             <Route exact path="/">
                 <Main />
             </Route>
         </Switch>
+        // ==========================================================
+        // <Switch>
+        //     <Route exact path="/:id/:name=:word/guess">
+        //         <GuesserView />
+        //     </Route>
+        //     <Route exact path="/:id/:name=:word">
+        //         <CreatorView />
+        //     </Route>
+        //     <Route exact path="/">
+        //         <Main />
+        //     </Route>
+        // </Switch>
     );
 }
 
--- client/src/views/CreatorView.jsx
+++ client/src/views/CreatorView.jsx
@@ -1,17 +1,81 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import Header from "../components/Header";
 import axios from "axios";
 import Leaderboard from "../components/Leaderboard";
+var CryptoJS = require("crypto-js");
 
 const CreatorView = () => {
-    const { id, name, word } = useParams();
+    // const { id, name, word } = useParams();
+    const [data, setData] = useState(null);
+    const { encryptedObj } = useParams();
+
+    useEffect(() => {
+        console.log(encryptedObj);
+
+        const bytes = CryptoJS.AES.decrypt(
+            decodeURIComponent(encryptedObj),
+            "secret-key"
+        );
+        const decryptedObj = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+        console.log(decryptedObj.name);
+        setData({
+            id: decryptedObj.id,
+            name: decryptedObj.name,
+            word: decryptedObj.word,
+        });
+        // const { id, name, word } = decryptedObject;
+        console.log(decryptedObj);
+    }, []);
+
+    // const encryptObj = (data) => {
+    //     let encryptedObject = CryptoJS.AES.encrypt(
+    //         JSON.stringify(data),
+    //         "secret-key"
+    //     ).toString();
+    //     //log encrypted data
+    //     console.log("Encrypt Data -");
+    //     console.log(encryptedObject);
+
+    // var bytes = CryptoJS.AES.decrypt(encryptedObject, "secret-key");
+    // var decryptedObject = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+    // console.log(decryptedObject);
+
+    // return encryptedObject;
 
     return (
         <div>
+            {data ? (
+                <div>
+                    <Header />
+                    {/* <h1>{data.name}'s Dashboard</h1> */}
+                    <div className="links">
+                        <h3>Creator Link:</h3>
+                        <a
+                            href={`http://localhost:3000/creator/${encryptedObj}`}
+                        >
+                            Creator Link
+                        </a>
+                    </div>
+                    <div className="links">
+                        <h3>Share Link:</h3>
+                        <a
+                            href={`http://localhost:3000/playgame/${encryptedObj}`}
+                        >
+                            Share Link
+                        </a>
+                    </div>
+                    {/* insert leader board component here */}
+                    <Leaderboard name={data.name} id={data.id} />
+                </div>
+            ) : (
+                <></>
+            )}
+            {/* 
             <Header />
-            <h1>{name}'s Dashboard</h1>
-            <div className="links">
+            <h1>Hello World</h1> */}
+            {/* <h1>{data.name}'s Dashboard</h1> */}
+            {/* <div className="links">
                 <h3>Creator Link:</h3>
                 <a href="">{`http://localhost:3000/${id}/${name}=${word}`}</a>
             </div>
@@ -20,9 +84,9 @@ const CreatorView = () => {
                 <a
                     href={`http://localhost:3000/${id}/${name}=${word}/guess`}
                 >{`http://localhost:3000/${id}/${name}=${word}/guess`}</a>
-            </div>
+            </div> */}
             {/* insert leader board component here */}
-            <Leaderboard name={name} id={id} />
+            {/* <Leaderboard name={data.name} id={data.id} /> */}
         </div>
     );
 };
